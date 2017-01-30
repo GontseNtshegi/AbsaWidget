844257e97d5e8963280a9d4ff43a801a514af38f
Ntshegi <ntshegg@lhc.local>
2017-01-30 16:10:20 +0200

    big change

--- public_html/bootloader.js
+++ public_html/bootloader.js
@@ -4,10 +4,10 @@
  * and open the template in the editor.
  */
 
-document.write('<link rel="”stylesheet”" type="”text/css”" href="”style.css" />');
-document.write('<script type="”text/javascript”" src="”widget.js”"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="style.css" />');
+document.write('<script type="text/javascript" src="widget.js"></script>');
 var myElement = document.getElementById('myFirstWidget');
 var JavaScriptCode = document.createElement('script');
-JavaScriptCode.setAttribute('type', 'text/javascript');
+JavaScriptCode.setAttribute("type", 'text/javascript');
 JavaScriptCode.setAttribute("src", 'http://widgets-gadges.com/widgetbook/chapter4/data.js');
 document.getElementById('myFirstWidget').appendChild(JavaScriptCode);
